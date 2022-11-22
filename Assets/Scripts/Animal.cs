e6192354def5b357a4f9462a85539c18e10b387a
d-yates <davidyates04@gmail.com>
2022-11-22 19:32:07 +0100

    Polymorphism

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Animal : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public string typeAnimal;
+
+    public virtual void Start() {
+        Debug.Log("A new animal is created : " + typeAnimal);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public virtual void Jump(){
+        Debug.Log("The " + typeAnimal + " is jump");
+    }
+
+    public void Move(){
+        Debug.Log("The " + typeAnimal + " is move forward");
+    }
+
+    public void Eat(){
+        Debug.Log("The " + typeAnimal + " eats");
     }
 }
