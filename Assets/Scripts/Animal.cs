71a67f329682394679df19e7c89d32e4b6712ab1
d-yates <davidyates04@gmail.com>
2022-11-22 21:33:27 +0100

    Encapsulation

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,31 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Animal : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public TextMeshPro uiText;
     public string typeAnimal;
 
-    public virtual void Start() {
-        Debug.Log("A new animal is created : " + typeAnimal);
+    // Encapsulation
+    private string m_NameAnimal;
+
+    public string nameAnimal
+    {
+        get { return m_NameAnimal; }
+        set
+        {
+            if (value.Length > 10)
+            {
+                Debug.LogError("The name is to long");
+            }
+            else
+            {
+                m_NameAnimal = value;
+            }
+        }
     }
 
-     // POLYMORPHISM
-    public virtual void Jump(float maxHeight){
+
+    public virtual void Start()
+    {
+        Debug.Log("A new " + typeAnimal +  " is created and name is : " + nameAnimal);
+        uiText.text = typeAnimal + "\n" + nameAnimal;
+    }
+
+    // POLYMORPHISM
+    public virtual void Jump(float maxHeight)
+    {
         Debug.Log("The " + typeAnimal + " it's jumping at a height of : " + maxHeight);
     }
 
-    public virtual void Jump(Vector3 jumpForce){
+    public virtual void Jump(Vector3 jumpForce)
+    {
         Debug.Log("The " + typeAnimal + " it's jumping at a force of : " + jumpForce);
     }
 
-    public void Move(){
+    public void Move()
+    {
         Debug.Log("The " + typeAnimal + " is move forward");
     }
 
-    public void Eat(){
+    public void Eat()
+    {
         Debug.Log("The " + typeAnimal + " eats");
     }
 }
