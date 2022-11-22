9f8cc569af49a2445e5cbbf3c5a18318e5cffe79
d-yates <davidyates04@gmail.com>
2022-11-22 18:55:47 +0100

    Inheritance
    Create Animal → Cat / Dog classes

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class Cat : Animal
+{
+    // Start is called before the first frame update
+    
+}
---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class Dog : Animal
+{
+    // Start is called before the first frame update
+    
+}
